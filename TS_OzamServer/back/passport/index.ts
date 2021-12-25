f4975ab62bc057bb9d26850a79f8325d869c44bd
criminal415 <denny415@naver.com>
2021-12-26 02:04:56 +0900

    [edit] passport 에러 해결

--- TS_OzamServer/back/passport/index.ts
+++ TS_OzamServer/back/passport/index.ts
@@ -3,22 +3,24 @@ import { isExpressionStatement } from 'typescript';
 import User from '../models/user';
 import local from './local';
 
-     
 export default () => {
-  passport.serializeUser<User, number>((user, done) => {
-      done(null, user.id);
+  passport.serializeUser((user: User, done) => {
+    done(null, user.id);
   });
   passport.deserializeUser(async (id: number, done) => {
-      try {
-          const user = await User.findOne({
-              where: { id },
-          })
-          return done(null, user);
-        } catch (err) {
-            console.error(err);
-            return done(err);
-        }
+    try {
+      const user = await User.findOne({
+        where: { id },
+      });
+      if (!user) {
+        return done(new Error('no user'));
       }
+      return done(null, user);
+    } catch (err) {
+      console.error(err);
+      return done(err);
+    }
   });
+
   local();
 };
--- TS_OzamServer/back/types/index.d.ts
+++ TS_OzamServer/back/types/index.d.ts
@@ -1,7 +1,12 @@
 import User from '../models/user';
-
+import IUser from '../models/user';
 declare module 'express-serve-static-core' {
   interface Request {
     user?: User;
+  }
+}
+declare global {
+  namespace Express {
+    export interface User extends IUser {}
   }
 }
