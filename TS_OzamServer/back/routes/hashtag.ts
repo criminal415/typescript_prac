fa2d81cdf0f1b3f18c1c153e00b42afdde22651a
criminal415 <denny415@naver.com>
2022-01-07 16:21:58 +0900

    [feat] hashtag router

---
+++ TS_OzamServer/back/routes/hashtag.ts
@@ -0,0 +1,59 @@
+import express from 'express';
+import Sequelize from 'sequelize';
+import { Hashtag, Image, Post } from '../models';
+import User from '../models/user';
+
+const router = express.Router();
+
+router.get('/:tag', async (req, res, next) => {
+  try {
+    let where = {};
+    if (req.query.lastId) {
+      where = {
+        id: {
+          [Sequelize.Op.lt]: req.query.lastId,
+        },
+      };
+    }
+    const posts = await Post.findAll({
+      where,
+      include: [
+        {
+          model: Hashtag,
+          where: { name: decodeURIComponent(req.params.tag) },
+        },
+        {
+          model: User,
+          as: 'Likers',
+          attributes: ['id', 'nickname'],
+        },
+        {
+          model: Image,
+        },
+        {
+          model: User,
+          as: 'Likers',
+          attributes: ['id'],
+        },
+        {
+          model: Post,
+          as: 'Retweet',
+          include: [
+            {
+              model: User,
+              attributes: ['id', 'nickname'],
+            },
+            {
+              model: Image,
+            },
+          ],
+          order: [['createdAt', 'DESC']],
+          limit: parseInt(JSON.stringify(req.query.limit).replace(/\"/g, ''), 10),
+        },
+      ],
+    });
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
+});
